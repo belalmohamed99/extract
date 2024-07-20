44e22d0d4dbb6f66b66b98141012042b41f5ee82
Belal mohamed <belal2003mohamed@gmail.com>
2024-07-20 19:03:45 +0300

    add extract files

---
+++ index.js
@@ -0,0 +1,28 @@
+const array = [
+  8,
+  "55",
+  [2, "hello world", { a: 2, b: 5 }, false],
+  {
+    arr: [true, 1, NaN, new Array(2, 33)],
+    test: null,
+    obj: { d: "moha", last: [2, false, undefined] },
+  },
+];
+const [
+  ,
+  ,
+  [
+    ,
+    ,
+    ,
+    {
+      arr: [, , , [, num]],
+    },
+  ],
+  {
+    obj: { d: nam },
+  },
+] = array;
+
+console.log(num);
+console.log(nam);
